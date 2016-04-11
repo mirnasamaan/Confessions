7be8cb5253c2283f72fcb2fcc84d3da30a828556
Mirna Samaan <m.ashraf@enlightworld.com>
2016-04-11 16:01:11 +0200

    Initial Commit (DataBinding)

---
+++ app/src/main/java/com/example/marvoot/testingandroid/View/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.marvoot.testingandroid.View;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.databinding.DataBindingUtil;
+
+import com.example.marvoot.testingandroid.R;
+import com.example.marvoot.testingandroid.ViewModel.MainViewModel;
+import com.example.marvoot.testingandroid.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements MainViewModel.DataListener {
+
+    private ActivityMainBinding binding;
+    private MainViewModel mainViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainViewModel = new MainViewModel(this, this);
+        binding.setViewModel(mainViewModel);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/marvoot/testingandroid/ViewModel/ViewModel.java
@@ -0,0 +1,9 @@
+package com.example.marvoot.testingandroid.ViewModel;
+
+/**
+ * Created by Marvoot on 4/11/2016.
+ */
+public interface ViewModel {
+
+    void destroy();
+}
